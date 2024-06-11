5937ad61bd2b732186d3a129ac51ca9718975131
SilverPug1 <sspizzapug@gmail.com>
2024-06-11 12:29:20 -0700

    Build Constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,5 +1,19 @@
 package frc.robot;
 
-public class BuildConstants {
-    
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FreshmanBot";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "9b6c30764684bf966b8244c1640c9a84213e3d5d";
+  public static final String GIT_DATE = "2024-06-11 15:19:10 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-06-11 15:23:25 EDT";
+  public static final long BUILD_UNIX_TIME = 1718133805582L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
 }
