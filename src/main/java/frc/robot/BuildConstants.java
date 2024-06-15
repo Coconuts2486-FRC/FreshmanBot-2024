4bd3dda74d0eddf35dfdb7f672a5042db40b4c80
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2024-06-15 16:03:13 -0700

    Updated version after successful build
    	modified:   src/main/java/frc/robot/BuildConstants.java
    	modified:   src/main/java/frc/robot/RobotContainer.java
    	modified:   src/main/java/frc/robot/subsystems/drive/ModuleIONutBlend.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "AdvantageKit_AdvancedSwerveDriveProject";
+  public static final String MAVEN_NAME = "BlendedSwerve_AK";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2024-06-02 11:33:31 EDT";
-  public static final long BUILD_UNIX_TIME = 1717342411542L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "a72201e667a4c833441d708a5731d1273e97dd38";
+  public static final String GIT_DATE = "2024-06-15 18:55:08 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-06-15 19:00:44 EDT";
+  public static final long BUILD_UNIX_TIME = 1718492444895L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
