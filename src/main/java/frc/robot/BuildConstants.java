a72201e667a4c833441d708a5731d1273e97dd38
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2024-06-15 15:55:08 -0700

    Attempt at a blended NEO/Falcon swerve drive

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "AdvantageKit_AdvancedSwerveDriveProject";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2024-06-02 11:33:31 EDT";
+  public static final long BUILD_UNIX_TIME = 1717342411542L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants() {}
+}
