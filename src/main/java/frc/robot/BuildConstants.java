e90ae2b63fec68dfd311184bedd5615d91b0a234
SilverPug1 <sspizzapug@gmail.com>
2024-06-24 16:02:53 -0700

    Add the intake code from elsewhere

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FreshmanBot-2024";
+  public static final String MAVEN_NAME = "FreshmanBot";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "807d2d769692fbf5a16abb8a2e7561a645f7db69";
-  public static final String GIT_DATE = "2024-06-18 10:48:02 EDT";
+  public static final String GIT_SHA = "e3147166338a79460316eb47e80247af22fc9517";
+  public static final String GIT_DATE = "2024-06-18 12:24:36 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-06-18 12:23:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1718727826424L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-06-24 13:23:24 EDT";
+  public static final long BUILD_UNIX_TIME = 1719249804430L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
