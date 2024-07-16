d7a10b336154f23c3fd80327664dca48cc4d75c3
OscarCDavis <orcarcdavis@gmail.com>
2024-07-16 16:29:58 -0700

    pushing

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FreshmanBot-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 37;
-  public static final String GIT_SHA = "ce2a1de3a8fc56ffdf17d1e3993fa3a3eb43dc36";
-  public static final String GIT_DATE = "2024-07-16 18:01:11 EDT";
+  public static final int GIT_REVISION = 38;
+  public static final String GIT_SHA = "238e57f074e258bafdfaa2ce2b401033bf532c0e";
+  public static final String GIT_DATE = "2024-07-16 19:06:09 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-07-16 19:02:41 EDT";
-  public static final long BUILD_UNIX_TIME = 1721170961306L;
+  public static final String BUILD_DATE = "2024-07-16 19:16:44 EDT";
+  public static final long BUILD_UNIX_TIME = 1721171804970L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
