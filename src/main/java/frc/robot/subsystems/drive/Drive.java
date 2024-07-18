66f31849fad3a068267b5e5a66804b1bbf354bd6
OscarCDavis <orcarcdavis@gmail.com>
2024-07-18 14:24:47 -0700

    fixed auto pid

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -71,7 +71,11 @@ public class Drive extends SubsystemBase {
         () -> kinematics.toChassisSpeeds(getModuleStates()),
         this::runVelocity,
         new HolonomicPathFollowerConfig(
-            MAX_LINEAR_SPEED, DRIVE_BASE_RADIUS, new ReplanningConfig()),
+          new PIDConstants(0.05, 0.0, 0.0),
+          new PIDConstants(0.05, 0.0, 0.0),
+            MAX_LINEAR_SPEED, 
+            DRIVE_BASE_RADIUS, 
+            new ReplanningConfig()),
         () -> DriverStation.getAlliance().isPresent()
             && DriverStation.getAlliance().get() == Alliance.Red,
         this);
